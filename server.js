a5b8ada3b92c417229a710c87f6d947845cf444a
Enes Ercan Enwar Hurmuzlu <enesalhurmuzi@gmail.com>
2025-02-05 19:51:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.get('*', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
